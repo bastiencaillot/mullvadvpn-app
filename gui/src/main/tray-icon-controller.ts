3d7038a100f83ba17f689253180b94622657bdf6
Oskar Nyberg <oskar@mullvad.net>
2021-10-07 09:44:54 +0200

    Detect tray color and use appropriate icon color

--- gui/src/main/tray-icon-controller.ts
+++ gui/src/main/tray-icon-controller.ts
@@ -1,6 +1,10 @@
+import { exec as execAsync } from 'child_process';
 import { nativeImage, NativeImage, Tray } from 'electron';
 import path from 'path';
+import { promisify } from 'util';
 import KeyframeAnimation from './keyframe-animation';
+
+const exec = promisify(execAsync);
 
 export type TrayIconType = 'unsecured' | 'securing' | 'secured';
 
@@ -9,16 +13,18 @@ export default class TrayIconController {
   private iconImages: NativeImage[] = [];
 
   constructor(
-    tray: Tray,
+    private tray: Tray,
     private iconTypeValue: TrayIconType,
     private useMonochromaticIconValue: boolean,
-  ) {
-    this.loadImages();
+  ) {}
+
+  public async init() {
+    await this.loadImages();
 
     const initialFrame = this.targetFrame();
     const animation = new KeyframeAnimation();
     animation.speed = 100;
-    animation.onFrame = (frameNumber) => tray.setImage(this.iconImages[frameNumber]);
+    animation.onFrame = (frameNumber) => this.tray.setImage(this.iconImages[frameNumber]);
     animation.play({ start: initialFrame, end: initialFrame });
 
     this.animation = animation;
@@ -35,13 +41,17 @@ export default class TrayIconController {
     return this.iconTypeValue;
   }
 
-  set useMonochromaticIcon(useMonochromaticIcon: boolean) {
-    this.useMonochromaticIconValue = useMonochromaticIcon;
-    this.loadImages();
+  public async reloadImages() {
+    await this.loadImages();
 
     if (this.animation && !this.animation.isRunning) {
       this.animation.play({ end: this.targetFrame() });
     }
+  }
+
+  public async setUseMonochromaticIcon(useMonochromaticIcon: boolean) {
+    this.useMonochromaticIconValue = useMonochromaticIcon;
+    await this.reloadImages();
   }
 
   public animateToIcon(type: TrayIconType) {
@@ -57,20 +67,59 @@ export default class TrayIconController {
     animation.play({ end: frame });
   }
 
-  private loadImages() {
+  private async loadImages() {
     const frames = Array.from({ length: 10 }, (_, i) => i + 1);
-    this.iconImages = frames.map((frame) => nativeImage.createFromPath(this.getImagePath(frame)));
+    this.iconImages = await Promise.all(
+      frames.map(async (frame) => nativeImage.createFromPath(await this.getImagePath(frame))),
+    );
   }
 
-  private getImagePath(frame: number) {
+  private async getImagePath(frame: number) {
     const basePath = path.resolve(path.join(__dirname, '../../assets/images/menubar icons'));
     const extension = process.platform === 'win32' ? 'ico' : 'png';
     let suffix = '';
     if (this.useMonochromaticIconValue) {
-      suffix = process.platform === 'darwin' ? 'Template' : '_white';
+      suffix = await this.monochromaticFileNameSuffix();
     }
 
     return path.join(basePath, process.platform, `lock-${frame}${suffix}.${extension}`);
+  }
+
+  private async monochromaticFileNameSuffix() {
+    switch (process.platform) {
+      case 'darwin':
+        return 'Template';
+      case 'win32': {
+        try {
+          const { stdout, stderr } = await exec(
+            'reg query HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize\\ /v SystemUsesLightTheme',
+          );
+
+          if (!stderr && stdout) {
+            // Split the output into rows
+            const rows = stdout.split('\n');
+            // Select the row that contains the registry entry result
+            const resultRow = rows.find((row) => row.includes('SystemUsesLightTheme'))?.trim();
+            // Split the row into words
+            const resultRowWords = resultRow?.split(' ').filter((word) => word !== '');
+            // Grab value which is last word on the result row
+            const value = resultRowWords && resultRowWords[resultRowWords.length - 1];
+
+            if (value) {
+              const parsedValue = parseInt(value);
+              return parsedValue === 1 ? '_black' : '_white';
+            }
+          }
+
+          return '_white';
+        } catch {
+          return '_white';
+        }
+      }
+      case 'linux':
+      default:
+        return '_white';
+    }
   }
 
   private targetFrame(): number {
